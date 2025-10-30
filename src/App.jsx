dd2909ddb40dc9b79270d7a155d7db5c4d0ed58c
pedro-henrique-oliveira <henriqueoliveiradasilvapedro1@gmail.com>
2025-10-30 11:32:05 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,26 @@ function App() {
 
   return (
     <>
-      <h1 className={styles.titulo}>Pedro</h1>
+      <nav className={styles.menu}>
+        <a href="#s1">Home</a>
+        <a href="#s2">Cards</a>
+        <a href="#s3">Video</a>
+        <a href="#s4">Contrato</a>
+      </nav>
+      <main>
+        <section>
+          <h2>sassao 1</h2>
+        </section>
+        <section>
+          <h2>sassao 2</h2>
+        </section>
+        <section>
+          <h2>sassao 3</h2>
+        </section>
+        <section>
+          <h2>sassao 4</h2>
+        </section>
+      </main>
     </>
   )
 }
