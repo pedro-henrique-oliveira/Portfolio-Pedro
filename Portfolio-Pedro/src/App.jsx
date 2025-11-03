4385783f0c1cd0a62b408f5a7e09955a0db0104c
pedro-henrique-oliveira <henriqueoliveiradasilvapedro1@gmail.com>
2025-11-03 11:40:12 -0300

    Add files via upload

---
+++ Portfolio-Pedro/src/App.jsx
@@ -0,0 +1,33 @@
+import styles from './App.module.css'
+
+function App() {
+
+// javascript
+
+  return (
+    <>
+      <nav className={styles.menu}>
+        <a href="#s1">Home</a>
+        <a href="#s2">Cards</a>
+        <a href="#s3">Video</a>
+        <a href="#s4">Contrato</a>
+      </nav>
+      <main>
+        <section>
+          <h2>sassao 1</h2>
+        </section>
+        <section>
+          <h2>sassao 2</h2>
+        </section>
+        <section>
+          <h2>sassao 3</h2>
+        </section>
+        <section>
+          <h2>sassao 4</h2>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css'
+import Perfil from '../public/foto1.png'
 
 function App() {
 
@@ -13,16 +14,25 @@ function App() {
         <a href="#s4">Contrato</a>
       </nav>
       <main>
-        <section>
-          <h2>sassao 1</h2>
+        <section className={styles.s1} id='s1'>
+          <div className={styles.left}>
+            <img className={styles.imgPerfil} src={Perfil} alt="Foto perfil"  />
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.title}>Pedro</h2>
+            <p className={styles.paragraph}>aluno do curso tecnico de desenvolvimento de Sistema pelo Sanai</p>
+          </div>
         </section>
-        <section>
+
+        <section id='s2'>
           <h2>sassao 2</h2>
         </section>
-        <section>
+
+        <section id='s3'>
           <h2>sassao 3</h2>
         </section>
-        <section>
+
+        <section id='s4'>
           <h2>sassao 4</h2>
         </section>
       </main>
