750778139efb0f2aab7f44cb2a7beb0222afd6fe
pedro-henrique-oliveira <henriqueoliveiradasilvapedro1@gmail.com>
2025-11-13 11:14:40 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
 import styles from './App.module.css'
+
 import Perfil from '../public/foto1.png'
 import facebookIcon from '../public/facebook.webp'
 import instagramIcon from '../public/instagram.webp'
 import GithubIcon from '../public/Github.png'
 import whatsAppIcon from '../public/whatsApp.webp'
-import HTMLImg from '/HTML.png'
-import CSSImg from '/CSS.png'
-import JSImg from '/js (1).png'
-import VITEImg from '/vite.png'
-import REACTImg from '/REACT.png'
-import VERCELImg from '/VERCEL.png'
+
+import { useState, useEffect } from 'react'
+import { Card } from './components/card'
 
 
 function App() {
+  const [dados, setDados] = useState([])
 
-// javascript
+  useEffect(() => {
+    fetch('/cardsInfo.json')
+    .then(response => response.json())
+    .then(data => {
+      setDados(data)
+    })
+  }, [])
+
+  const defaultPhoneNumber = '5541987216563'
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setFormData({...formData, [name]: value})
+  }
+
+
+  const handleZap = () => {
+    const {name, email, message} = formData
+
+    const urlzap = `https://api.whatsapp.com/send?phone=${defaultPhoneNumber}&text=
+    Nome:%20${name}%0D%0A
+    Email:%20${email} %0D%0A
+    Mensagem:%20${message}%0D%0A`
+
+    window.open(urlzap, "_blank")
+  }
+
 
   return (
     <>
@@ -38,45 +69,41 @@ function App() {
         <section className={styles.s2} id='s2'>
           <h2 className={styles.tectitle}>Tecnologias</h2>
           <div className={styles.wrapCards}>
-              <div className={styles.card}>
-                  <img width={80} src={HTMLImg} alt="HTML" />
-                  <h3>HTML</h3>
-                  <p>HTML é a sigla para Hypertext Markup Language (Linguagem de Marcação de Hipertexto) e é a linguagem de marcação padrão usada para criar e estruturar páginas da web</p>
-              </div>
-              <div className={styles.card}>
-                  <img width={80} src={CSSImg} alt="CSS" />
-                  <h3>CSS</h3>
-                  <p>CSS (Cascading Style Sheets) é uma linguagem de estilo usada para descrever a apresentação visual de um documento escrito em linguagem de marcação como HTML</p>
-              </div>
-              <div className={styles.card}>
-                  <img width={80} src={JSImg} alt="JS" />
-                  <h3>JS</h3>
-                  <p>JavaScript é uma linguagem de programação leve, interpretada e versátil, usada para tornar páginas da web dinâmicas e interativas</p>
-              </div>
-              <div className={styles.card}>
-                  <img width={80} src={VITEImg} alt="VITE" />
-                  <h3>VITE</h3>
-                  <p>Vite é uma ferramenta moderna de construção de front-end projetada para fornecer uma experiência de desenvolvimento mais rápida e eficiente para web</p>
-              </div>
-              <div className={styles.card}>
-                  <img width={80} src={REACTImg} alt="REACT" />
-                  <h3>REACT</h3>
-                  <p>React é uma biblioteca JavaScript de código aberto para construir interfaces de usuário (UI)</p>
-              </div>
-              <div className={styles.card}>
-                  <img width={80} src={VERCELImg} alt="VERCEL" />
-                  <h3>VERCEL</h3>
-                  <p>A Vercel é uma plataforma de nuvem popular e de alto desempenho, projetada especificamente para desenvolvedores front-end</p>
-              </div>
+              {dados.map((item) => {
+                return(
+                  <div key={item.id}>
+                  <Card tec={item.tecnologia} image={item.imagem} text={item.texto}/>
+                  </div>
+                )
+              })}
           </div>
         </section>
 
         <section id='s3'>
-          <h2>sassao 3</h2>
+          <h2>Github repositorios</h2>
         </section>
 
-        <section id='s4'>
-          <h2>sassao 4</h2>
+        <section id='s5' className={styles.s5}>
+          <h5 className={styles.titulo}></h5>
+          <div className={styles.wrapJogos}>
+            <div>
+          
+                
+            </div>
+          </div>
+        </section>
+
+        <section id='s4' className={styles.s4}>
+          <h2>CONTATO</h2>
+          <div className={styles.formData}>
+            <label htmlFor="name">Informe seu nome</label>
+            <input type="text" id='name'  name='name' value={formData.name} onChange={handleChange} required />
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" id='email' name='email' value={formData.email} onChange={handleChange} required />
+            <label htmlFor="message">Informe uma mensagem</label>
+            <textarea name="message" id="message" value={formData.message} onChange={handleChange} cols="30" rows="10" required ></textarea>
+            <button onClick={handleZap}>Eviar mensagem</button>
+          </div>
         </section>
       </main>
       <footer className={styles.rodape}>
